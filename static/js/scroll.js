713feb5ba47b3cc1f67d3f12fe25c27edf8f04f1
Alberto Strappazzon <strappazzon@protonmail.com>
2020-06-21 20:06:41 +0200

    CSP: Remove inline JS

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,14 +1,14 @@
-let topBtn = document.getElementById('scroll-up');
+const topBtn = document.getElementById('scroll-up');
 
-window.onscroll = function() { scroll() };
-
-function scroll() {
+window.onscroll = function () {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		topBtn.style.display = 'block';
-	} else {
+		topBtn.style.display = 'block'; }
+	else {
 		topBtn.style.display = 'none';
 	}
-}
+};
+
+topBtn.addEventListener('click', scrollUp, false);
 
 function scrollUp() {
 	document.body.scrollTop = 0;
