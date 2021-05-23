a9441f68629a7a63cb93e6fa787d307f4daed00b
Alberto Strappazzon <a@strappazzon.xyz>
2021-05-23 23:20:46 +0200

    Make terminal buttons work

---
+++ static/js/terminal.js
@@ -0,0 +1,34 @@
+const TERM_MINMZ_BTN = document.getElementById('term-min');
+const TERM_MAXMZ_BTN = document.getElementById('term-max');
+const TERM_CLOSE_BTN = document.getElementById('term-close');
+let isMinimized = false;
+
+TERM_MINMZ_BTN.addEventListener('click', e => {
+	var term = document.querySelector('.terminal');
+	var termCnt = document.querySelector('.terminal .content');
+
+	if (isMinimized == false) {
+		term.classList.add('mh-0');
+		termCnt.classList.add('d-n');
+		isMinimized = true;
+	}
+});
+
+TERM_MAXMZ_BTN.addEventListener('click', e => {
+	var term = document.querySelector('.terminal');
+	var termCnt = document.querySelector('.terminal .content');
+
+	if (isMinimized) {
+		term.classList.remove('mh-0');
+		termCnt.classList.remove('d-n');
+		isMinimized = false;
+	}
+});
+
+TERM_CLOSE_BTN.addEventListener('click', e => {
+	var term = document.querySelector('.terminal');
+	var dlCnt = document.querySelector('.downloads__container');
+
+	term.parentNode.removeChild(term);
+	dlCnt.classList.add('ml-0');
+});
