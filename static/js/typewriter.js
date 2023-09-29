54f94de0aa56d6a8e740218dd6cb59144d3aca27
Alberto Strappazzon <a@strappazzon.xyz>
2023-09-30 00:59:13 +0200

    Format JS

--- static/js/typewriter.js
+++ static/js/typewriter.js
@@ -1,18 +1,17 @@
 let i = 0;
 const TYPE = document.querySelector('#typewriter');
-const TEXT = TYPE.dataset.text; // https://stackoverflow.com/a/37880192
+const TEXT = TYPE.dataset.text;
 const SPEED = 125;
 
 // https://www.w3schools.com/howto/howto_js_typewriter.asp
 function typeWriter() {
-	if (i < TEXT.length) {
-		TYPE.innerHTML += TEXT.charAt(i);
-		i++;
-		setTimeout(typeWriter, SPEED);
-	}
+  if (i < TEXT.length) {
+    TYPE.innerHTML += TEXT.charAt(i);
+    i++;
+    setTimeout(typeWriter, SPEED);
+  }
 }
 
-// Remove "hidden" attribute
 TYPE.hasAttribute('hidden') && TYPE.removeAttribute('hidden');
 
 typeWriter();
