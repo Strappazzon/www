7dde7806292cdd36b858192633d6a1e3672b790f
Strappazzon <im@strappazzon.xyz>
2025-05-04 09:33:08 +0200

    html_tags plugin: Replace Markdown image with title with `<figure>`

--- _plugins/html_tags.rb
+++ _plugins/html_tags.rb
@@ -1,10 +1,11 @@
 #
-# Jekyll hooks that wrap Markdown input inside HTML tags:
+# Jekyll hooks that replace Markdown syntax elements with HTML tags:
+#   <figure>
 #   <ins>
 #   <mark>
 #   <kbd>
 #
-# Inspired by PyMdown extension:
+# Some inspired by PyMdown extension:
 #   https://facelessuser.github.io/pymdown-extensions/extensions/caret/
 #   https://facelessuser.github.io/pymdown-extensions/extensions/mark/
 #   https://facelessuser.github.io/pymdown-extensions/extensions/keys/
@@ -12,13 +13,16 @@
 # This plugin contains work from:
 #   https://github.com/maximevaillancourt/digital-garden-jekyll-template/blob/5feb308/_plugins/markdown-highlighter.rb
 #   https://github.com/stephenreid321/stephenreid/blob/9ab9f00/app/jekyll_blog/_plugins/custom_markdown_processor.rb
+#   https://github.com/jonlpersson/ed-latimore/blob/3e29fef/site/_plugins/img-tag-transform.rb
 #
 # USAGE
+#   ![Alt](https://example.com/#image.jpg "Caption")
 #   ==important text==
 #   ^^inserted text^^
 #   ++CTRL++ + ++A++
 #
 # OUTPUT
+#   <figure><img src="https://example.com/#image.jpg" alt="Alt"><figcaption>Caption</figcaption></figure>
 #   <mark>important text</mark>
 #   <ins>inserted text</ins>
 #   <kbd>CTRL</kbd> + <kbd>A</kbd>
@@ -26,7 +30,24 @@
 
 module Jekyll
   module HTMLTags
-    VERSION = '1.0.0'.freeze
+    VERSION = '1.1.0'.freeze
+
+    def self.figure(input)
+      input.content.gsub!(/(?<!`)!\[(.*)\]\(([^\)]+)?"(.*)"\)(?!`)/i) do
+        alt     = Regexp.last_match(1)
+        url     = Regexp.last_match(2)
+        caption = Regexp.last_match(3)
+
+        # rubocop:disable Layout/ArrayAlignment
+        %W[
+          <figure>
+            <img alt="#{alt}" src="#{url}">
+            <figcaption>#{caption}</figcaption>
+          </figure>
+        ].join(' ')
+        # rubocop:enable Layout/ArrayAlignment
+      end
+    end
 
     def self.ins(input)
       input.content.gsub!(/(?<!`)\^{2}([^ ](.*?)?[^ \^])\^{2}(?!`)/i) { |m| "<ins>#{m[2..-3]}</ins>" }
@@ -43,20 +64,22 @@ module Jekyll
 end
 
 # Hook: Blog collection
-Jekyll::Hooks.register [:blog], :pre_render do |i|
-  Jekyll::HTMLTags.ins(i)
-  Jekyll::HTMLTags.mark(i)
-  Jekyll::HTMLTags.kbd(i)
+Jekyll::Hooks.register [:blog], :pre_render do |h|
+  Jekyll::HTMLTags.figure(h)
+  Jekyll::HTMLTags.ins(h)
+  Jekyll::HTMLTags.mark(h)
+  Jekyll::HTMLTags.kbd(h)
 end
 
-# Hook: All website pages
-Jekyll::Hooks.register [:pages], :pre_render do |i|
-  # Match only Markdown documents inside 'pages/'
-  next unless i.path.match?(/^pages\/([\w\d\-]+(\/)?)+\.md$/i)
+# Hook: Website pages
+Jekyll::Hooks.register [:pages], :pre_render do |h|
+  # Match only Markdown files inside 'pages/'
+  next unless h.path.match?(/^pages\/([\w\d\-]+(\/)?)+\.md$/i)
 
-  Jekyll::HTMLTags.ins(i)
-  Jekyll::HTMLTags.mark(i)
-  Jekyll::HTMLTags.kbd(i)
+  Jekyll::HTMLTags.figure(h)
+  Jekyll::HTMLTags.ins(h)
+  Jekyll::HTMLTags.mark(h)
+  Jekyll::HTMLTags.kbd(h)
 end
 
 Jekyll.logger.info 'Plugin:', "#{File.basename(__FILE__)} #{Jekyll::HTMLTags::VERSION} loaded."
